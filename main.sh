c39019940069a3424270efabd83f45639011825a
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-13 16:50:32 +1000

    Use `--force` for `cargo-binstall` in case bin is not cached
    but `$CARGO_HOME/binstall/crates-v1.json` is cached
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- main.sh
+++ main.sh
@@ -123,8 +123,8 @@ cargo_binstall() {
     # As a result, http will be disabled, and it will also set
     # min tls version to be 1.2
     case "${version}" in
-        latest) cargo binstall --secure --no-confirm "$tool" ;;
-        *) cargo binstall --secure --no-confirm --version "$version" "$tool" ;;
+        latest) cargo binstall --force --secure --no-confirm "$tool" ;;
+        *) cargo binstall --force --secure --no-confirm --version "$version" "$tool" ;;
     esac
 }
 
