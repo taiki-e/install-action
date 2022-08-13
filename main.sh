d6991ee7cda17c400104366e81aeabc15ce849c0
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-13 16:54:24 +1000

    Rm extra empty space reported by shfmt
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- main.sh
+++ main.sh
@@ -108,7 +108,7 @@ install_cargo_binstall() {
         x cargo binstall -V
     else
         info "cargo-binstall already installed on in ${cargo_bin}/cargo-binstall, upgrading"
-        cargo binstall --secure --no-confirm  --version '>=0.12.0' cargo-binstall
+        cargo binstall --secure --no-confirm --version '>=0.12.0' cargo-binstall
     fi
 }
 cargo_binstall() {
