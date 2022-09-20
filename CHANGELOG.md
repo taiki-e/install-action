a0deac80c03a49c2008c137acae1d1b49716cb5a
Taiki Endo <te316e89@gmail.com>
2022-09-21 01:17:45 +0900

    Update wasmtime to 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ Note: In this file, do not use the hard wrap in the middle of a sentence for com
 
 ## [Unreleased]
 
+- Update `wasmtime@latest` to 1.0.0.
+
 ## [1.13.2] - 2022-09-16
 
 - Update `cargo-udeps@latest` to 0.1.33.
--- main.sh
+++ main.sh
@@ -407,7 +407,7 @@ for tool in "${tools[@]}"; do
             ;;
         wasmtime)
             # https://github.com/bytecodealliance/wasmtime/releases
-            latest_version="0.40.1"
+            latest_version="1.0.0"
             repo="bytecodealliance/${tool}"
             case "${version}" in
                 latest) version="${latest_version}" ;;
