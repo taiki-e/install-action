8db8546a9c4311c0848140af11d81765d04f33ab
Taiki Endo <te316e89@gmail.com>
2023-01-14 00:02:59 +0900

    Add note about composite action

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// This is a script that just calls the bash script that does the main
+// processing of the action. It works like a composite action that calls
+// a single bash script.
+//
+// This was originally a trick adopted to make bash script-based actions work
+// without docker before composite actions were supported. However, due to
+// various problems with composite actions, this trick is still needed:
+// - https://github.com/actions/runner/issues/665
+// - https://github.com/actions/runner/issues/1947
+// - https://github.com/actions/runner/issues/2185
+
 const { execFileSync } = require('child_process');
 
 function main() {
