42e74b55ab1a623e62d9805ceb0ffb055bf933cd
Taiki Endo <te316e89@gmail.com>
2023-07-09 13:11:42 +0900

    Revert "Update release scripts (take 3)"
    This reverts commit be8c41d56e1b3b1faee7041f2eafa0bc9975f1c3.
    
    ```
    + git push origin refs/heads/v2
    To https://github.com/taiki-e/install-action
     ! [rejected]        v2 -> v2 (non-fast-forward)
    error: failed to push some refs to 'https://github.com/taiki-e/install-action'
    hint: Updates were rejected because the tip of your current branch is behind
    hint: its remote counterpart. Integrate the remote changes (e.g.
    hint: 'git pull ...') before pushing again.
    hint: See the 'Note about fast-forwards' in 'git push --help' for details.
    ```

--- tools/publish.sh
+++ tools/publish.sh
@@ -102,7 +102,53 @@ if [[ -n "${tags}" ]]; then
     x git commit -m "Release ${version}"
 fi
 
-x git tag "${tag}"
-# TODO: the following still assumes admin permissions
-x git push origin main
-x git push origin --tags
+tools=()
+for tool in tools/codegen/base/*.json; do
+    tools+=("$(basename "${tool%.*}")")
+done
+# Aliases
+tools+=(nextest)
+# Not manifest-based
+tools+=(valgrind)
+
+(
+    set -x
+
+    git tag "${tag}"
+    git push origin main
+    git push origin --tags
+
+    major_version_tag="v${version%%.*}"
+    git checkout -b "${major_version_tag}"
+    git push origin refs/heads/"${major_version_tag}"
+    if git --no-pager tag | grep -Eq "^${major_version_tag}$"; then
+        git tag -d "${major_version_tag}"
+        git push --delete origin refs/tags/"${major_version_tag}"
+    fi
+    git tag "${major_version_tag}"
+    git checkout main
+    git branch -d "${major_version_tag}"
+)
+
+for tool in "${tools[@]}"; do
+    (
+        set -x
+        git checkout -b "${tool}"
+        sed -i -e "s/required: true/required: false/g" action.yml
+        sed -i -e "s/# default: #publish:tool/default: ${tool}/g" action.yml
+        git add action.yml
+        git commit -m "${tool}"
+        git push origin -f refs/heads/"${tool}"
+        if git --no-pager tag | grep -Eq "^${tool}$"; then
+            git tag -d "${tool}"
+            git push --delete origin refs/tags/"${tool}"
+        fi
+        git tag "${tool}"
+        git checkout main
+        git branch -D "${tool}"
+    )
+done
+
+set -x
+
+git push origin --tags
