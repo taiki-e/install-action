e52d622f2da1ea75a2857b12b1ba8679d8a42576
John Vandenberg <jayvdb@gmail.com>
2024-03-27 22:23:09 +0800

    Add cargo-spellcheck (#423)

--- tools/ci/tool-list.sh
+++ tools/ci/tool-list.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$0")"/../..
 # They don't provide prebuilt binaries for musl or old glibc host.
 glibc_pre_2_34_incompat=(
     cargo-cyclonedx
+    cargo-spellcheck
     xbuild
 )
 glibc_pre_2_31_incompat=(
