0a085838dee5cf98977a5acd893509927e781846
Jiahao XU <Jiahao_XU@outlook.com>
2024-10-15 22:52:29 +1100

    Fix checkout-manifest-schema-branch.sh fmt
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- tools/checkout-manifest-schema-branch.sh
+++ tools/checkout-manifest-schema-branch.sh
@@ -10,9 +10,9 @@ branch="manifest-schema-${schema_version}"
 git worktree add --force "${1?}"
 cd "$1"
 
-if git fetch origin "$branch"; then
+if git fetch origin "${branch}"; then
     git checkout "origin/${branch}" -B "${branch}"
-elif ! git checkout "$branch"; then
+elif ! git checkout "${branch}"; then
     # New branch with no history. Credit: https://stackoverflow.com/a/13969482
     git checkout --orphan "${branch}"
     git rm -rf . || true
