59a3819eee2c7268d216dbe794f1120b880458eb
Taiki Endo <te316e89@gmail.com>
2025-01-28 15:57:16 +0900

    clean scripts

--- tools/ci/checkout-manifest-schema-branch.sh
+++ tools/ci/checkout-manifest-schema-branch.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: Apache-2.0 OR MIT
-
-set -euxo pipefail
-
-cd "$(dirname "$0")"
+set -CeEuo pipefail
+IFS=$'\n\t'
+trap -- 's=$?; printf >&2 "%s\n" "${0##*/}:${LINENO}: \`${BASH_COMMAND}\` exit with ${s}"; exit ${s}' ERR
+cd -- "$(dirname -- "$0")"
 
 version="$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name == "install-action-manifest-schema") | .version')"
-if [[ ${version} == 0.* ]]; then
-  schema_version="0.$(echo "${version}" | cut -d '.' -f 2)"
+if [[ "${version}" == "0."* ]]; then
+  schema_version="0.$(cut -d. -f2 <<<"${version}")"
 else
-  schema_version="$(echo "${version}" | cut -d '.' -f 2)"
+  schema_version="$(cut -d. -f1 <<<"${version}")"
 fi
 branch="manifest-schema-${schema_version}"
 
 git worktree add --force "${1?}"
-cd "$1"
+cd -- "$1"
 
 if git fetch origin "${branch}"; then
   git checkout "origin/${branch}" -B "${branch}"
